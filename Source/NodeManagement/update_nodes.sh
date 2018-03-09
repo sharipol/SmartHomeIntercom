c70c034a149c1862efebd4543dc40ef658cc8abe
Glen-N-Anderson <glen.nowel.anderson@gmail.com>
2018-03-08 16:50:33 -0800

    Added MAC address to written output

--- Source/NodeManagement/update_nodes.sh
+++ Source/NodeManagement/update_nodes.sh
@@ -6,5 +6,8 @@ room=$1
 host=`hostname`
 occupants=$2
 current_time=`date '+%H:%M:%S'`
+etho0=`ifconfig -a | grep ether`
+arr=($eth0)
+macaddr=${arr[1]}
 
-echo $room $host $occupants $current_time >> $roompath/last_occupied
+echo $room $host $macaddr $occupants $current_time >> $roompath/last_occupied
