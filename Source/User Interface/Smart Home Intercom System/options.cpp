361e6a1097add9bbfa71e985d3e6a87cbfbbf36d
davijord <32402650+davijord@users.noreply.github.com>
2018-05-05 22:17:59 +0000

    Updated in requirements and function purposes

--- Source/User Interface/Smart Home Intercom System/options.cpp
+++ Source/User Interface/Smart Home Intercom System/options.cpp
@@ -1,3 +1,5 @@
+//This is the main code for the options menu
+
 #include "options.h"
 #include "ui_options.h"
 #include <QProcess>
@@ -19,11 +21,13 @@ options::~options()
     delete ui;
 }
 
+//Because the options are changed on command, this button is unused.
 void options::on_apply_button_clicked()
 {
     
 }
 
+//Lets the user cancel out.
 void options::on_pushButton_clicked()
 {
     close();
@@ -31,6 +35,7 @@ void options::on_pushButton_clicked()
 
 void options::on_brightness_slider_valueChanged(int value)
 {
+	//Allows for brightness of the screen to be adjusted.
 	QProcess process;
 	QString program = QString::fromLatin1("sudo sh -c \"echo %1 > /sys/class/backlight/rpi_backlight/brightness\"").arg(value);
 
@@ -48,6 +53,7 @@ void options::on_brightness_slider_valueChanged(int value)
 
 void options::on_volume_slider_valueChanged(int value)
 {
+	//allows for volume control
     QProcess process;
 	QString program = QString::fromLatin1("amixer sset PCM -M %1\%").arg(value);
 	process.start(program);
@@ -63,6 +69,7 @@ void options::on_volume_slider_valueChanged(int value)
 }
 
 void options::on_defaultMode_activated(int value) {
+	//added security to lockout users from admin mode
 		if (value == 2) {
 		MyInputPanel *inputPanel = new MyInputPanel;
 		inputPanel->show();
