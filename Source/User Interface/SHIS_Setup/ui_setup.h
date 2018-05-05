c1ad795edeef52f08f5bcf07fe82e1327b10f474
davijord <32402650+davijord@users.noreply.github.com>
2018-05-05 14:26:58 -0700

    Updating Setup to most recent versions

--- Source/User Interface/SHIS_Setup/ui_setup.h
+++ Source/User Interface/SHIS_Setup/ui_setup.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'setup.ui'
 **
-** Created by: Qt User Interface Compiler version 4.8.6
+** Created by: Qt User Interface Compiler version 4.8.7
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -18,7 +18,6 @@
 #include <QtGui/QHeaderView>
 #include <QtGui/QMainWindow>
 #include <QtGui/QPushButton>
-#include <QtGui/QStatusBar>
 #include <QtGui/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -28,9 +27,8 @@ class Ui_Setup
 public:
     QWidget *centralWidget;
     QPushButton *accept;
-    QComboBox *comboBox;
+    QComboBox *Room;
     QCheckBox *guestbox;
-    QStatusBar *statusBar;
 
     void setupUi(QMainWindow *Setup)
     {
@@ -47,21 +45,21 @@ public:
         font.setBold(true);
         font.setWeight(75);
         accept->setFont(font);
-        comboBox = new QComboBox(centralWidget);
-        comboBox->setObjectName(QString::fromUtf8("comboBox"));
-        comboBox->setGeometry(QRect(50, 30, 491, 71));
+        Room = new QComboBox(centralWidget);
+        Room->setObjectName(QString::fromUtf8("Room"));
+        Room->setGeometry(QRect(50, 30, 491, 71));
+        QFont font1;
+        font1.setPointSize(20);
+        Room->setFont(font1);
         guestbox = new QCheckBox(centralWidget);
         guestbox->setObjectName(QString::fromUtf8("guestbox"));
-        guestbox->setGeometry(QRect(80, 150, 179, 33));
-        QFont font1;
-        font1.setPointSize(18);
-        guestbox->setFont(font1);
+        guestbox->setGeometry(QRect(50, 140, 179, 33));
+        QFont font2;
+        font2.setPointSize(18);
+        guestbox->setFont(font2);
         guestbox->setIconSize(QSize(40, 40));
         guestbox->setTristate(false);
         Setup->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(Setup);
-        statusBar->setObjectName(QString::fromUtf8("statusBar"));
-        Setup->setStatusBar(statusBar);
 
         retranslateUi(Setup);
 
@@ -72,6 +70,19 @@ public:
     {
         Setup->setWindowTitle(QApplication::translate("Setup", "Setup", 0, QApplication::UnicodeUTF8));
         accept->setText(QApplication::translate("Setup", "Accept", 0, QApplication::UnicodeUTF8));
+        Room->clear();
+        Room->insertItems(0, QStringList()
+         << QApplication::translate("Setup", "Living Room", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Kitchen", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Hallway", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Garage", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Master Bedroom", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Office", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Bedroom 1", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Bedroom 2", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Dining Room", 0, QApplication::UnicodeUTF8)
+         << QApplication::translate("Setup", "Porch", 0, QApplication::UnicodeUTF8)
+        );
         guestbox->setText(QApplication::translate("Setup", "     Guest Only", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
