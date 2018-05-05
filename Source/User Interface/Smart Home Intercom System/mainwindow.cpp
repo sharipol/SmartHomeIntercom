ad0d5855028cdb4aa02d2dbf9718f956d59586d2
davijord <32402650+davijord@users.noreply.github.com>
2018-05-05 16:53:45 -0700

    Updated to make device able to receiver without interaction

--- Source/User Interface/Smart Home Intercom System/mainwindow.cpp
+++ Source/User Interface/Smart Home Intercom System/mainwindow.cpp
@@ -11,7 +11,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QProcess *audioreceive = new QProcess(this);
+    QProcess *videoreceive = new QProcess(this);
 
+    QString receiveVideo = QString::fromLatin1("gst-launch-1.0 udpsrc port=5555 ! h264parse ! omxh264dec ! autovideosink");
+    QString receiveAudio = QString::fromLatin1("gst-launch-1.0 udpsrc port=5000 caps=\"application/x-rtp\" ! rtpopusdepay ! opusdec ! autoaudiosink");
+
+    videoreceive->start(receiveVideo);
+    audioreceive->start(receiveAudio);
+    
 }
 
 MainWindow::~MainWindow()
@@ -34,17 +42,17 @@ void MainWindow::on_Video_page_pressed()
     QProcess *videosend = new QProcess(this);
     QProcess *audioreceive = new QProcess(this);
     QProcess *audiosend = new QProcess(this);
-    QProcess *killall = new QProcess(this);
+    QProcess *killallprocess = new QProcess(this);
     
     QTimer *timer = new QTimer(this);
     
-    QString sendVideo = QString::fromLatin1("gst-launch-1.0 rpicamsrc bitrate=2097152 preview-opacity=255 preview-x=0 preview-y=0 preview-w=480 preview-h=270 fullscreen=false rotation=180 sensor-mode=5 annotation-mode=0x020c ! omxh264enc target-bitrate=2097152 control-rate=variable ! video/x-h264,width=480,height=270,framerate=25/1,profile=high ! h264parse ! udpsink host=192.168.0.1 port=5555");
+    QString sendVideo = QString::fromLatin1("gst-launch-1.0 rpicamsrc bitrate=2097152 preview-opacity=255 preview-x=0 preview-y=0 preview-w=480 preview-h=270 fullscreen=false rotation=180 sensor-mode=5 annotation-mode=0x020c ! omxh264enc target-bitrate=2097152 control-rate=variable ! video/x-h264,width=480,height=270,framerate=25/1,profile=high ! h264parse ! udpsink host=192.168.0.2 port=5555");
     QString receiveVideo = QString::fromLatin1("gst-launch-1.0 udpsrc port=5555 ! h264parse ! omxh264dec ! autovideosink");
     QString killCV =  QString::fromLatin1("kill $(ps axuww | grep \"python Desktop/OpenCV.py\" | awk \'{print $2}\' | sed -n 1p)");
     QString openCV = QString::fromLatin1("python Desktop/OpenCV.py");
-    QString sendAudio = QString::fromLatin1("gst-launch-1.0 alsasrc device=hw:1 ! audioconvert ! opusenc ! rtpopuspay ! udpsink host=192.168.0.1 port=5000");
-    QString receiveAudio = QString::fromLatin1("gst-launch-1.0 udpsrc port=5000 caps="application/x-rtp" ! rtpopusdepay ! opusdec ! autoaudiosink");
-    QString killall = QString::fromLatin1("kill $(ps -a | grep gst | awk '{print $1}')")
+    QString sendAudio = QString::fromLatin1("gst-launch-1.0 alsasrc device=hw:1 ! audioconvert ! opusenc ! rtpopuspay ! udpsink host=192.168.0.2 port=5000");
+    QString receiveAudio = QString::fromLatin1("gst-launch-1.0 udpsrc port=5000 caps=\"application/x-rtp\" ! rtpopusdepay ! opusdec ! autoaudiosink");
+    QString killall = QString::fromLatin1("kill $(ps -a | grep gst | awk '{print $1}')");
     
     //process->start(killCV);
     //Need some dedicated conditional to know whether to send or receive.
@@ -63,7 +71,7 @@ void MainWindow::on_Audio_page_pressed()
     QProcess *audioreceive = new QProcess(this);
     QProcess *audiosend = new QProcess(this);
     QString sendAudio = QString::fromLatin1("gst-launch-1.0 alsasrc device=hw:1 ! audioconvert ! opusenc ! rtpopuspay ! udpsink host=192.168.0.1 port=5000");
-    QString receiveAudio = QString::fromLatin1("gst-launch-1.0 udpsrc port=5000 caps="application/x-rtp" ! rtpopusdepay ! opusdec ! autoaudiosink");
+    QString receiveAudio = QString::fromLatin1("gst-launch-1.0 udpsrc port=5000 caps=\"application/x-rtp\" ! rtpopusdepay ! opusdec ! autoaudiosink");
     
     audioreceive->start(receiveAudio);
     audiosend->start(sendAudio);
