cbec6ee96c8180e79401fc8df6a4a7caf68f2709
davijord <32402650+davijord@users.noreply.github.com>
2018-05-05 22:02:33 +0000

    Commenting met requirement

--- Source/User Interface/Smart Home Intercom System/main.cpp
+++ Source/User Interface/Smart Home Intercom System/main.cpp
@@ -1,3 +1,7 @@
+//This meets the "embedded" requirement per the update with Kevin:
+//"A chromeless full-screened window is fine for the embedded requirement."
+//showFullScreen forces that with Qt.
+
 #include "mainwindow.h"
 #include <QApplication>
 
